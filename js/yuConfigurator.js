33d8c3a6bb2aba1ceea6dd842c856f1a14638397
Julius Überall <juliusueberall@gmx.de>
2023-09-09 19:44:43 +0100

    Adds prototype for three.js viewer

---
+++ js/yuConfigurator.js
@@ -0,0 +1,76 @@
+//Connect to div that stores the configurator
+const container = document.getElementById('yu_configurator');
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(50, container.clientWidth / container.clientHeight, 0.1, 1000);
+camera.position.z = 3;
+
+// Create a renderer and set the viewer in HTML
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setClearColor(0xffffff);
+container.appendChild(renderer.domElement);
+
+//LIGHT SETUP
+const ambient = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(ambient);
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(-3, 5, 2);
+scene.add(light);
+
+//Add keyboard interactions
+document.addEventListener('keydown', function(event) {
+    // Check if the pressed key is 'K' (or 'k')
+    if (event.key === 'K' || event.key === 'k') {
+        //Create curve
+        const points = [];
+        points.push( new THREE.Vector3( RandomInRange(-5,5), RandomInRange(-5,5), RandomInRange(-10,0)) );
+        points.push( new THREE.Vector3( RandomInRange(-5,5), RandomInRange(-5,5), RandomInRange(-10,0)) );
+        points.push( new THREE.Vector3( RandomInRange(-5,5), RandomInRange(-5,5), RandomInRange(-10,0)) );
+
+        const curve = new QuadraticBezierCurve(points[0],points[1], points[2]);
+
+        // Create a tube (extrude) from the line's geometry
+        const tubeGeometry = new THREE.TubeGeometry(curve, 100, 0.03, 8, false);
+        const tubeMaterial = new THREE.MeshPhongMaterial({ color: 0xff00ff });
+        const tube = new THREE.Mesh(tubeGeometry, tubeMaterial);
+        scene.add(tube);
+    }
+});
+
+
+//Animate and render
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+};
+
+// Start the animation loop
+animate();
+
+
+class QuadraticBezierCurve extends THREE.Curve {
+    constructor(v0, v1, v2) {
+        super();
+        this.v0 = v0;
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    getPoint(t) {
+        const oneMinusT = 1 - t;
+        return new THREE.Vector3(
+            oneMinusT * oneMinusT * this.v0.x + 2 * oneMinusT * t * this.v1.x + t * t * this.v2.x,
+            oneMinusT * oneMinusT * this.v0.y + 2 * oneMinusT * t * this.v1.y + t * t * this.v2.y,
+            oneMinusT * oneMinusT * this.v0.z + 2 * oneMinusT * t * this.v1.z + t * t * this.v2.z
+        );
+    }
+}
+
+function RandomInRange(start, end){
+    return (end - start) * Math.random() + start;
+}
