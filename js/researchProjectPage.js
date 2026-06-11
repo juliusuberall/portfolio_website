9046f6620f3e3e090df73f922f495ed468d75ab8
Julius Überall <jueberall@mpi-inf.mpg.de>
2026-06-11 13:22:22 +0200

    Add citations for BEH

--- js/researchProjectPage.js
+++ js/researchProjectPage.js
@@ -2,13 +2,16 @@
 const divsWithRef = document.querySelectorAll('div[ref]');
 
 divsWithRef.forEach(element => {
-    const aTagRefLink = document.createElement('a');
-    const refId = element.getAttribute('ref')
-    aTagRefLink.href = '#' + refId
-    const refElement = document.getElementById(refId)
-    const olParent= refElement.closest('ol');
-    const refNumber = Array.from(olParent.children).indexOf(refElement) +1;
-    aTagRefLink.text = ' [' + refNumber + ']'
-    aTagRefLink.classList.add('content_ref');
-    element.appendChild(aTagRefLink)
+    const refIds = element.getAttribute('ref').split(' ')
+    refIds.forEach(refId => {
+        const aTagRefLink = document.createElement('a');
+        aTagRefLink.href = '#' + refId
+        const refElement = document.getElementById(refId)
+        const olParent = refElement.closest('ol');
+        const refNumber = Array.from(olParent.children).indexOf(refElement) +1;
+        aTagRefLink.text = ' [' + refNumber + ']'
+        aTagRefLink.title = refElement.textContent.trim();
+        aTagRefLink.classList.add('content_ref');
+        element.appendChild(aTagRefLink)
+    })
 });
